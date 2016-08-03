86f9857e4a29fc11654f8ad9083a36b2dc31d4f7
zhanlei01_58 <zhanlei01@58ganji.com>
2016-08-03 15:28:13 +0800

    file

--- src/api.js
+++ src/api.js
@@ -5,8 +5,8 @@ export default {
 		authenticated: false
 	},
 	token: '',
-	// login: url + 'src/data/data.json',
-	login: 'http://124.207.17.52/HSUSER/login/doLogin.action',
+	login: url + 'src/data/data.json',
+	// login: 'http://124.207.17.52/HSUSER/login/doLogin.action',
 	register: url + 'src/data/data.json',
 	signup: url + 'signup',
 	logout: url + 'logout'
